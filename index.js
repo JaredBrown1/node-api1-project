e4716032545a2d2e5d19713eea25200d7b364164
Jared Brown <jaredbrown1@hotmail.com>
2020-05-05 21:38:02 -0700

    post request working

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const shortid = require("shortid");
 
 const server = express();
 
 server.use(express.json());
 
+let users = [];
+
 server.get("/", (req, res) => {
   res.json({ message: "hello world!" });
 });
 
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+
+  userInfo.id = shortid.generate();
+  users.push(userInfo);
+
+  res.status(201).json(userInfo);
+});
+
+//-----------------------------------------------------
 const PORT = 5000;
 
 server.listen(PORT, () => {
